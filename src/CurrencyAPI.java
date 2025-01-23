e2be7fb1d38751837c2c52484501fb77fa2dd456
astoncodes <dannyaston916@gmail.com>
2025-01-23 14:02:03 -0400

    update 2

--- src/CurrencyAPI.java
+++ src/CurrencyAPI.java
@@ -1,5 +1,4 @@
 package src;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
---
+++ src/main.java
@@ -0,0 +1,9 @@
+package src;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Currency Changer!");
+        // Initialize the ConsoleUI or service
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.start();
+    }
+}
